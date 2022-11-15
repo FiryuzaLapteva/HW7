c2c30f7fb44bd93ea5b4bba3a4a828f077fb26f8
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-11-15 19:17:52 +0300

    controller chanched

--- controller.py
+++ controller.py
@@ -1,4 +1,6 @@
 from view import Value_type, Get_mode, Print_result, Get_rational_value, Get_complex_value
+from logger import calc_logger
+import modul_sum, modul_div, modul_multi, modul_sub
 
 def Launch_project():
     if Value_type():
@@ -6,21 +8,29 @@ def Launch_project():
         num1 = Get_complex_value() #- ДЛЯ КОМПЛЕКСНЫХ ЧИСЕЛ - ОК
         print('Введите второе число')
         num2 = Get_complex_value()
+        operat = Get_mode()
+        if operat == '+':
+            result = modul_sum.summa(num1, num2)
+        elif operat == '-':
+            result = modul_sub.sub(num1, num2)
+        elif operat == '*':
+            result = modul_multi.Mult(num1, num2)
+        elif operat == '/':
+            result = modul_div.Division(num1, num2)
     else:
         print('Введите первое число')  #- ДЛЯ РAЦИОНАЛЬНЫХ ЧИСЕЛ - ОК
         num1 = Get_rational_value()
         print('Введите второе число')
         num2 = Get_rational_value()
-
-    operat = Get_mode()
-    if operat == '+':
-        result = num1 + num2
-    elif operat == '-':
-        result = num1 - num2
-    elif operat == '*':
-        result = num1 * num2
-    elif operat == '/':
-        result = num1 / num2
+        operat = Get_mode()
+        if operat == '+':
+            result = modul_sum.summa_compl(num1, num2)
+        elif operat == '-':
+            result = modul_sub.sub_compl(num1, num2)
+        elif operat == '*':
+            result = modul_multi.Mult_compl(num1, num2)
+        elif operat == '/':
+            result = modul_div.Division_compl(num1, num2)
 
     Print_result(operat, num1, num2, result)
 
